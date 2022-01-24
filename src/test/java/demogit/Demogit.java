24db17adeb591d63f82959b27e8f01ccf4058523
som <som@som-HP>
2022-01-24 18:46:06 +0530

    user1 first commit

--- src/test/java/demogit/Demogit.java
+++ src/test/java/demogit/Demogit.java
@@ -5,6 +5,6 @@ import org.testng.annotations.Test;
 public class Demogit {
 @Test
 public void demogit() {
-	System.out.println("Hi user1 is here");
+	System.out.println("Hi user1 is commit here");
 }
 }
