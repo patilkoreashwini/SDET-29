98518cf4aa204cd56f81eef3c911d8e4aef669ff
som <som@ashwini>
2022-01-28 13:34:38 +0530

    this is user1 commit

---
+++ src/test/java/demogit/Ashwini.java
@@ -0,0 +1,12 @@
+package demogit;
+
+import org.testng.annotations.Test;
+
+public class Ashwini {
+	
+		@Test
+		public void demogit() {
+			System.out.println("Hi user1 is commited here");
+		}
+		}
+
